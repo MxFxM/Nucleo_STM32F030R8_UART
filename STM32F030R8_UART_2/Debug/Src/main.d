1eacd2a710346dcad58d4c912ecc6cfa47c30537
Max-Felix Müller <32713448+MxFxM@users.noreply.github.com>
2019-11-21 16:55:07 +0100

    a copy of UART_1 but made by me

---
+++ STM32F030R8_UART_2/Debug/Src/main.d
@@ -0,0 +1,15 @@
+Src/main.o: ../Src/main.c ../Inc/stm32f030x6.h ../Inc/core_cm0.h \
+ ../Inc/core_cmInstr.h ../Inc/cmsis_gcc.h ../Inc/core_cmFunc.h \
+ ../Inc/system_stm32f0xx.h
+
+../Inc/stm32f030x6.h:
+
+../Inc/core_cm0.h:
+
+../Inc/core_cmInstr.h:
+
+../Inc/cmsis_gcc.h:
+
+../Inc/core_cmFunc.h:
+
+../Inc/system_stm32f0xx.h:
